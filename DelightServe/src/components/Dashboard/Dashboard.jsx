7a750fbef1a2371d48ffb46b1e92a0d34f602eef
prabingeorge <prabingeorge@gmail.com>
2026-01-22 18:54:12 +0530

    Added timer code

--- DelightServe/src/components/Dashboard/Dashboard.jsx
+++ DelightServe/src/components/Dashboard/Dashboard.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useState, useContext } from "react";
 import Images from "../Images/Images";
 import api from "../../contexts/APIContext";
-import { CartContext } from "../../contexts/Cart"
+import { CartContext } from "../../contexts/Cart";
+import { Timer } from "../Timer/Timer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBowlingBall } from '@fortawesome/free-solid-svg-icons';
 import "./index.css";
 
 const Dashboard = () => {
   const [categoriesList, setCategoriesList] = useState([]);
+  const [isTimerRunning, setTimerRunning] = useState(false);
 
-   const { selectedCategoryId } = useContext(CartContext);
+  const { selectedCategoryId } = useContext(CartContext);
 
   const apiURL = import.meta.env.VITE_API_URL;
   useEffect(() => {
     const fetchData = async () => {
+      setTimerRunning(true);
       try {
         const response = await api.post(apiURL + "/api/user/categories-list-by-id", { categoryId: selectedCategoryId });
         const { data } = response;
         setCategoriesList([...data]);
+        setTimerRunning(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -29,7 +35,11 @@ const Dashboard = () => {
       <div className="dashboard">
         <div>
           <div className="images-container">
-            <label className="no-data">No data is available.</label>
+            {isTimerRunning && <p className="timer">
+              <FontAwesomeIcon icon={faBowlingBall} size="10x" spin style={{ color: '#ed81f7' }} />
+              We are fecthing data for you, Kindly do wait <Timer isTimerRunning={isTimerRunning} />
+            </p>}
+            {!isTimerRunning && <label className="no-data">No data is available.</label>}
           </div>
         </div>
       </div>
